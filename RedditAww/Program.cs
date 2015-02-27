a5f901c06da8f0c7b9c0b4880579e11b2c0e3caa
James Brown <james---b@hotmail.co.uk>
2015-02-27 23:26:52 +0000

    Initial Commit

---
+++ RedditAww/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using RedditSharp;
+
+namespace RedditAww
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reddit reddit = new Reddit();
+            var subreddit = reddit.GetSubreddit("/r/aww");
+
+            foreach (var post in subreddit.New.Take(25))
+            {
+                Console.WriteLine(post.Title);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
