06c0fd7a7448a0e5549ebbf3bee1156792f815a5
James Brown <james---b@hotmail.co.uk>
2015-03-01 20:45:30 +0000

    Queries the imgur API for the image type

--- RedditAww/Program.cs
+++ RedditAww/Program.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 
 using RedditSharp;
-using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RedditAww
 {
@@ -14,8 +16,10 @@ namespace RedditAww
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a subbreddit: ");
+            var subredditStr = Console.ReadLine();
             Reddit reddit = new Reddit();
-            var subreddit = reddit.GetSubreddit("/r/aww");
+            var subreddit = reddit.GetSubreddit(subredditStr);
             
             using (WebClient client = new WebClient())
             {
@@ -23,13 +27,61 @@ namespace RedditAww
                 {
                     if (!post.IsSelfPost)
                     {
-                        Console.WriteLine("Downloading : " + post.Url);
-                        Directory.CreateDirectory("E:/James/Test/" + post.Author.FullName);
-                        client.DownloadFileAsync(post.Url, @"E:/James/Test/" + post.Author.FullName + "/test.png");
+                        string imageUrl = post.Url.ToString();
+                        
+                        if(imageUrl.Contains("imgur.com"))
+                        {
+                            string imageId = "";
+
+                            // Extract the image ID from the URL
+                            if (imageUrl.Contains("i.imgur.com"))
+                            {
+                                imageId = imageUrl.Substring(imageUrl.Length - 11, 7);
+                            }
+                            else
+                            {
+                                imageId = imageUrl.Substring(imageUrl.Length - 7, 7);
+                                Console.WriteLine("Hello! {0}", imageId);
+                            }
+
+                            // Send a request to the imgur API for more information about the image
+                            // using the image ID
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.imgur.com/3/image/id/" + imageId);
+                            request.Headers.Add("Authorization", "Client-ID 8c44ab1a312f478");
+                            request.Method = WebRequestMethods.Http.Get;
+                            request.Accept = "application/json";
+
+                            // Read the response from the server
+                            Stream response = request.GetResponse().GetResponseStream();
+                            StreamReader reader = new StreamReader(response);
+                            string responseFromServer = reader.ReadToEnd();
+                            reader.Close();
+                            response.Close();
+
+                            // Parse the response into JSON, and take only the parts we are interested in
+                            JObject imageSearch = JObject.Parse(responseFromServer);
+
+                            string imageType = imageSearch["data"]["type"].ToString();
+                            imageType = imageType.Substring(imageType.Length - 4, 4);
+
+                            // If the URL is NOT of form i.imgur.com    i.e. imgur.com/XXXXXXX
+                            if(!imageUrl.Contains("i.imgur.com"))
+                            {
+                                imageUrl = "http://i.imgur.com/" + imageId + "." + imageType;
+                            }
+
+                            Console.WriteLine("Downloading : " + post.Url + "     Author : " + post.Author.FullName);
+                            Console.WriteLine("Post: " + post.Title);
+
+                            // Create the directory for the image and download it
+                            Directory.CreateDirectory("E:/James/" + subredditStr + "/" + post.Author.FullName);
+                            client.DownloadFileAsync(post.Url, @"E:/James/" + subredditStr + "/" + post.Author.FullName + "/" + imageId + "." + imageType);
+                        }
                     }
                 }
             }
-            
+
+            Console.WriteLine("Complete!");
             Console.ReadLine();
         }
     }
